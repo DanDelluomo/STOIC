cdf5e2edf6b09d624c9f082e2e1a00c28eb15f6d
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2020-07-17 11:05:15 -0400

    built out Email class and fixed styling in class names to CapWords

--- mailer.py
+++ mailer.py
@@ -10,14 +10,23 @@ class Email:
         self.password = constants.password
         server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         server_ssl.ehlo() # optional, called by login()
-        server_ssl.login(self.username, self.password)  
+        server_ssl.login(self.username, self.password)
     
-    def send(self):
-        display_string = principles.Stoic_Principles().display()
-        print("SENDING: ", display_string)
-        server_ssl.sendmail(self.username, self.username, 
-                            display_string)
+    def get_email_list(self):
+        emails = []
+        with open('email_list.txt', 'r') as f:
+            for email in f.readlines():
+                if '\n' in email:
+                    email = email[:email.index('\n')]
+                emails.append(email)
+        return emails
 
+    def send(self, email_list):
+        display_string = principles.Stoic_Principles().display() 
+        for address in email_list:
+            print("SENDING TO: ", address)
+            server_ssl.sendmail(self.username, address, display_string)
+            
     def end_mailserver(self):
         #server_ssl.quit()
         server_ssl.close()
