6f890d3c47ae7db68d4b2c7d7a2d1d6411418d42
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2020-07-17 15:17:38 -0400

    built out logging functionality of past_reminder.py

--- past_reminder.py
+++ past_reminder.py
@@ -8,16 +8,26 @@ class PastReminder:
     def cycle(self):
         """Print each reflection and take user input."""
         print('\n')
-        reflections = {}
+        self.analysis = {}
         for reflection in self.reflections:
             print('Ponder this...' + '\n' + reflection)
-            reflections[reflection] = input('Have you improved this? ')
-        return reflections
-        
-        
+            self.analysis[reflection] = input('Have you improved this? ')
+    
+    def log(self):
+        """Log results in reflect_log.txt."""
+        with open('reflect_log.txt', 'a') as f:
+            for principle in self.analysis:
+                response = self.analysis[principle]
+                if '\n' in principle:
+                    principle = principle[:principle.index('\n')]
+                log_string = principle + ': \n\t\t\t' + response + '\n\n'
+                f.write(log_string)
+
+          
 if __name__ == '__main__':
     reminders = PastReminder()
     reminders.cycle()
+    reminders.log()
 
 
         
